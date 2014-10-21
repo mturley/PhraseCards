9dbcb392d577e135c19213536ea0cd2f99bc0743
colbyjstone <colbyjstone@gmail.com>
2014-10-21 14:59:02 -0400

    Fix posting to api

--- project/app.js
+++ project/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('./api', api);
+app.use('/api', api);
 require('./app/routes.js')(app);
 
 app.listen(port);
