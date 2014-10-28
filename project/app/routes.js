c70b046109be8a38e84d34d85a556628b3c9bd3c
Daniel <dchoi@umass.edu>
2014-10-28 16:05:49 -0400

    began working with signup

--- project/app/routes.js
+++ project/app/routes.js
@@ -8,14 +8,14 @@ module.exports = function(app,passport) {
       res.render('about.ejs');
     })
 
-		.get('/signup', function(req, res) {
+	.get('/signup', function(req, res) {
 			res.render('signup.ejs');
 		})
 
 		// process the login form
     .post('/', passport.authenticate('local-login', {
-successRedirect : '/about', // redirect to the secure profile section
-failureRedirect : '/', // redirect back to the signup page if there is an error
-failureFlash : true // allow flash messages
-}));
+		successRedirect : '/about', // redirect to the secure profile section
+		failureRedirect : '/', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+		}));
 }
