75b7352b5ba51a10a60b420c9cf956ae926b8ffd
Daniel <dcchoi1234@gmail.com>
2014-10-30 19:29:27 -0400

    conflict

--- project/app/routes.js
+++ project/app/routes.js
@@ -1,27 +1,30 @@
 module.exports = function(app,passport) {
 	app
     .get('/', function(req, res) {
-		  res.render('index.ejs', { message: req.flash('loginMessage') });
+		  res.render('index.ejs');
 	   })
+    .get('/login', function(req, res) {
+		  res.render('login.ejs', { message: req.flash('loginMessage') });
+	 })
+	.get('/signup', function(req, res) {
+			res.render('signup.ejs');
+		})
 
     .get('/about', function(req, res) {
       res.render('about.ejs');
     })
-	.get('/signup', function(req, res) {
-			res.render('signup.ejs');
-		})
 	.get('/logout', function(req, res) {
         req.logout();
-        res.redirect('/');
+        res.redirect('/login');
     })
 		// process the login form
-    .post('/', passport.authenticate('local-login', {
-		successRedirect : '/about', // redirect to the secure profile section
-		failureRedirect : '/', // redirect back to the signup page if there is an error
+    .post('/login', passport.authenticate('local-login', {
+		successRedirect : '/', // redirect to the secure profile section
+		failureRedirect : '/login', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 		}))
     .post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/about', // redirect to the secure profile section
+        successRedirect : '/', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
