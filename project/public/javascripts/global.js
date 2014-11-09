9d4524e34b92746572318d71e835e09be42d40ec
colbyjstone <colbyjstone@gmail.com>
2014-11-09 15:49:10 -0500

    Add lobby card flip

--- project/public/javascripts/global.js
+++ project/public/javascripts/global.js
@@ -1,6 +1,15 @@
 
-	$('#debug_menu').hide();
+$('#debug_menu').hide();
 
-	$('#debug_icon').click(function(){
-		$('#debug_menu').toggle();
-	});
+$('#debug_icon').click(function(){
+  $('#debug_menu').toggle();
+});
+
+$('#new_game_container').click(function(){
+  $('#new_game_card').toggleClass('flip');
+});
+
+$('.flipback').click(function(e){
+  e.preventDefault();
+  $('#new_game_card').toggleClass('flip');
+});
