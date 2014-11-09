912ee2c33a4ab9be939ef6d48539a8fbd0d41a95
Mike Turley <mike@miketurley.com>
2014-11-09 13:43:35 -0500

    converting api.js tabs to spaces

--- project/api.js
+++ project/api.js
@@ -1,83 +1,83 @@
 var express = require('express'),
-	app = express(),
-	mongoose = require('mongoose'),
-	dbURL = require('./config/database.js'),
-	db = mongoose.connect(dbURL.url),
-	User = require('./app/models/user'),
-	Game = require('./app/models/game'),
-	router = express.Router();
+  app = express(),
+  mongoose = require('mongoose'),
+  dbURL = require('./config/database.js'),
+  db = mongoose.connect(dbURL.url),
+  User = require('./app/models/user'),
+  Game = require('./app/models/game'),
+  router = express.Router();
 
 
 router
-	.route('/users')//chaining post and get
-		.post(function(req, res){
-			var user = new User();
-			user.email = req.body.email;
-			user.username = req.body.username;
-			user.save(function(err){
-				if(err)
-					res.send(err);
-				res.json({message : 'User created'});
-			});
-		})
-		.get(function(req,res){
-			User.find(function(err,users){
-				if(err)
-					res.send(err);
-				res.json(users);
-			})
-		});
+  .route('/users')//chaining post and get
+    .post(function(req, res){
+      var user = new User();
+      user.email = req.body.email;
+      user.username = req.body.username;
+      user.save(function(err){
+        if(err)
+          res.send(err);
+        res.json({message : 'User created'});
+      });
+    })
+    .get(function(req,res){
+      User.find(function(err,users){
+        if(err)
+          res.send(err);
+        res.json(users);
+      })
+    });
 
 
 router
-	.route('/users/:user_id')
-		.get(function(req,res){	//get the user given the id
-			User.findById(req.params.user_id,function(err,user){
-				if(err)
-					res.send(err);
-				res.json(user);
-			});
-		})
-		.put(function(req,res){	//remove the user given the id
-			User.findById(req.params.user_id,function(err,user){
-				if(err)
-					res.send(err);
+  .route('/users/:user_id')
+    .get(function(req,res){ //get the user given the id
+      User.findById(req.params.user_id,function(err,user){
+        if(err)
+          res.send(err);
+        res.json(user);
+      });
+    })
+    .put(function(req,res){ //remove the user given the id
+      User.findById(req.params.user_id,function(err,user){
+        if(err)
+          res.send(err);
 
-				user.email = req.body.email;
-				user.username = req.body.username;
-				
-				// save the user
-				user.save(function(err) {
-				if (err)
-					res.send(err);
+        user.email = req.body.email;
+        user.username = req.body.username;
+        
+        // save the user
+        user.save(function(err) {
+        if (err)
+          res.send(err);
 
-				res.json({ message: 'User updated!' });
-				});
-			});
-		})
-		.delete(function(req,res){	//remove the user given the id
-			User.remove({
-				_id: req.params.user_id
-			}, function(err, bear) {
-			if (err)
-				res.send(err);
+        res.json({ message: 'User updated!' });
+        });
+      });
+    })
+    .delete(function(req,res){  //remove the user given the id
+      User.remove({
+        _id: req.params.user_id
+      }, function(err, bear) {
+      if (err)
+        res.send(err);
 
-			res.json({ message: 'Successfully deleted' });
-			});
-		});
+      res.json({ message: 'Successfully deleted' });
+      });
+    });
 
 router
-	.route('/search')
-		.get(function(req,res){			 
-			User.find({ 'local.nickname': req.query['nickname'] }, function (err, users) {
-  				if (err)
-  					res.send(err);
-  				res.render('search.ejs',{
-  				Users : users
-  				});
-			})
-			
-		});
+  .route('/search')
+    .get(function(req,res){      
+      User.find({ 'local.nickname': req.query['nickname'] }, function (err, users) {
+          if (err)
+            res.send(err);
+          res.render('search.ejs',{
+          Users : users
+          });
+      })
+      
+    });
 
 
 
