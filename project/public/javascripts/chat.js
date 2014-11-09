f1fc284678cb65ab6cc155e1f71fa4bd21fbc775
colbyjstone <colbyjstone@gmail.com>
2014-11-09 15:03:10 -0500

    Animate chat

--- project/public/javascripts/chat.js
+++ project/public/javascripts/chat.js
@@ -8,12 +8,12 @@ $('#chatform').submit(function(){
 });
 
 function updateScroll(){
-  var element = $("#chat_container");
+  var element = document.getElementById("chat_container");
   element.scrollTop = element.scrollHeight;
 }
 
 socket.on('chat message', function(msg){
   // Replace with user info
-  $('#chat_container').append('<div class="chat_row"><img src="http://placehold.it/200x200" alt="" class="round_img" /> <strong>Username: </strong> ' + msg + "</div>");
+  $('#chat_container').append('<div class="chat_row animated zoomIn"><img src="http://placehold.it/200x200" alt="" class="round_img" /> <strong>Username: </strong> ' + msg + "</div>");
   updateScroll();
 });
