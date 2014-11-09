9757f80e949bd001dba211f165044ad3f043f554
colbyjstone <colbyjstone@gmail.com>
2014-11-09 14:26:43 -0500

    chat.js spacing

--- project/app/routes.js
+++ project/app/routes.js
@@ -1,43 +1,43 @@
 var gravatar = require('node-gravatar');
 
 module.exports = function(app,passport) {
-	app
+  app
     .get('/', function(req, res) {
-		  res.render('index.ejs', { message: req.flash('loginMessage')
-			});
-	   })
-		.get('/profile', isLoggedIn, function(req, res) {
-			res.render('profile.ejs',{
-				// get the user out of session and pass to template
-				user : req.user,
-				avatar : gravatar.get(req.user.local.email)
-			});
-		})
-		.get('/lobby', isLoggedIn, function(req, res) {
-			res.render('lobby.ejs', {
-				user : req.user,
-				avatar : gravatar.get(req.user.local.email)
-			});
-		})
-		.get('/game', isLoggedIn, function(req, res) {
-			res.render('game.ejs', {
-				user : req.user,
-				avatar : gravatar.get(req.user.local.email)
-			});
-		})
-	.get('/signup', function(req, res) {
-			res.render('signup.ejs', { message: req.flash('signupMessage') });
-		})
-	.get('/logout', function(req, res) {
+      res.render('index.ejs', { message: req.flash('loginMessage')
+      });
+     })
+    .get('/profile', isLoggedIn, function(req, res) {
+      res.render('profile.ejs',{
+        // get the user out of session and pass to template
+        user : req.user,
+        avatar : gravatar.get(req.user.local.email)
+      });
+    })
+    .get('/lobby', isLoggedIn, function(req, res) {
+      res.render('lobby.ejs', {
+        user : req.user,
+        avatar : gravatar.get(req.user.local.email)
+      });
+    })
+    .get('/game', isLoggedIn, function(req, res) {
+      res.render('game.ejs', {
+        user : req.user,
+        avatar : gravatar.get(req.user.local.email)
+      });
+    })
+  .get('/signup', function(req, res) {
+      res.render('signup.ejs', { message: req.flash('signupMessage') });
+    })
+  .get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
     })
-		// process the login form
+    // process the login form
     .post('/', passport.authenticate('local-login', {
-		successRedirect : '/lobby', // redirect to the secure profile section
-		failureRedirect : '/', // redirect back to the signup page if there is an error
-		failureFlash : true // allow flash messages
-		}))
+    successRedirect : '/lobby', // redirect to the secure profile section
+    failureRedirect : '/', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+    }))
     .post('/signup', passport.authenticate('local-signup', {
         successRedirect : '/lobby', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
--- project/public/javascripts/chat.js
+++ project/public/javascripts/chat.js
@@ -2,24 +2,24 @@ var socket = io();
 var messages= [];
 
 $('#chatform').submit(function(){
-	socket.emit('chat message', $('#message').val());
-	$('#message').val('');
-	return false;
+  socket.emit('chat message', $('#message').val());
+  $('#message').val('');
+  return false;
 });
 
 socket.on('chat message', function(msg){
-	if (messages.length <=0  || messages.length <= 2) {
-		messages.push(msg);
-	}else {
-		messages.splice(0,1);
-		messages.push(msg);
-	}
-	// Replace with user info
-	$('.chat_message1').html('<span class="chat_message"><img src="http://placehold.it/100x100" class="round_img"/> Username:  ' + messages[0] + '</span>');
-	if (messages.length > 1) {
-		$('.chat_message2').html('<span class="chat_message"><img src="http://placehold.it/100x100" class="round_img"/> Username:  ' + messages[1] + '</span>');
-		if (messages.length > 2) {
-			$('.chat_message3').html('<span class="chat_message"><img src="http://placehold.it/100x100" class="round_img"/> Username:  ' + messages[2] + '</span>');
-		}
-	}
+  if (messages.length <=0  || messages.length <= 2) {
+    messages.push(msg);
+  }else {
+    messages.splice(0,1);
+    messages.push(msg);
+  }
+  // Replace with user info
+  $('.chat_message1').html('<span class="chat_message"><img src="http://placehold.it/100x100" class="round_img"/> Username:  ' + messages[0] + '</span>');
+  if (messages.length > 1) {
+    $('.chat_message2').html('<span class="chat_message"><img src="http://placehold.it/100x100" class="round_img"/> Username:  ' + messages[1] + '</span>');
+    if (messages.length > 2) {
+      $('.chat_message3').html('<span class="chat_message"><img src="http://placehold.it/100x100" class="round_img"/> Username:  ' + messages[2] + '</span>');
+    }
+  }
 });
