f40e9553a4b35ec7b27ef5e2c121c39911bad011
rhromada <rhromada@umass.edu>
2014-11-23 15:44:14 -0500

    fuzzy search work

--- project/public/javascripts/profile.js
+++ project/public/javascripts/profile.js
@@ -2,9 +2,13 @@ $('.friend-filter').keyup(function (event){
 	var re = new RegExp('^' + $('.friend-filter').val());
 	$('#friend > li').each(function (index) {
       if (!re.test($('.potential_friend').text())) {
+      	console.log("hide");
+      	console.log(('.potential_friend').text());
         $('.potential_friend').hide();
       }
       else {
+      	console.log("show");
+      	console.log(('.potential_friend').text());
         $('.potential_friend').show();
       }
     });
