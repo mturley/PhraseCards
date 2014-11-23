7135e8332ee0e195e11ea328f8581d1895dbad9a
Mike Turley <mike@miketurley.com>
2014-11-22 23:54:02 -0500

    moved api.js to app/api.js, moved mongodb connection to app.js and pass it into api.js as a module paramater. this way, we can use the database outside the api (i.e. in sockets) without connecting to it twice. also cleaned up app.js, removing some redundant require calls and accidental global variables (careful with semicolons, guys)

--- project/app.js
+++ project/app.js
@@ -1,13 +1,15 @@
 var express      = require('express'),
     app          = express(),
-    api          = require('./api'),
     path         = require('path'),
     cookieParser = require('cookie-parser'),
     bodyParser   = require('body-parser'),
-    routes       = require('./app/routes'),
     passport     = require('passport'),
     flash        = require('connect-flash'),
     session      = require('express-session'),
+    mongodb      = require('mongodb'),
+    mongoose     = require('mongoose'),
+    dbURL        = require('./config/database.js'),
+    db           = mongoose.connect(dbURL.url),
     port         = process.env.PORT || 8080;
 
 
@@ -21,12 +23,15 @@ app.set('views', path.join(__dirname, 'views'))
    .use(passport.initialize())
    .use(passport.session()) // persistent login sessions
    .use(flash()) // use connect-flash for flash messages stored in session
-   .use('/api', api);
+   .use('/api', require('./app/api')(db));
+
 require('./config/passport')(passport);
 
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 var io = require('socket.io').listen(app.listen(port));
+
+console.log('Magic happens on port ' + port);
 
 // Incoming Socket Connections
 io.on('connection', function(socket){
@@ -38,5 +43,3 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
 });
-
-console.log('Magic happens on port ' + port);
