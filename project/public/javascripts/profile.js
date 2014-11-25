574e7838efc19a332f6f50bce1c89f7727baeaeb
rhromada <rhromada@umass.edu>
2014-11-25 17:01:00 -0500

    fifuzzy search update

--- project/public/javascripts/profile.js
+++ project/public/javascripts/profile.js
@@ -1,16 +1,14 @@
 $('.friend-filter').keyup(function (event){
 	var re = new RegExp('^' + $('.friend-filter').val());
-	$('#friend > ul').each(function () {
+	$('#friend > li .friend_name').each(function () {
 		console.log(re);
       	if (!re.test($(this).text())) {
-      		console.log("hide");
-      		console.log(('.potential_friend').text());
-        	$('.potential_friend').hide();
+      		//console.log("hide: "+$(this).text());
+        	$(this).parent().hide();
       	}
       	else {
-      		console.log("show");
-      		console.log(('.potential_friend').text());
-        	$('.potential_friend').show();
+      		//console.log("show: "+$(this).text());
+        	$(this).parent().show();
       	}
     });
 }); 
