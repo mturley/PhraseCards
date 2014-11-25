862d9884931cfc85b8d0afaf7a91834f19784a9c
rhromada <rhromada@umass.edu>
2014-11-25 16:28:06 -0500

    fixed ul to li in profile

--- project/public/javascripts/profile.js
+++ project/public/javascripts/profile.js
@@ -1,15 +1,16 @@
 $('.friend-filter').keyup(function (event){
 	var re = new RegExp('^' + $('.friend-filter').val());
-	$('#friend > li').each(function (index) {
-      if (!re.test($('.potential_friend').text())) {
-      	console.log("hide");
-      	console.log(('.potential_friend').text());
-        $('.potential_friend').hide();
-      }
-      else {
-      	console.log("show");
-      	console.log(('.potential_friend').text());
-        $('.potential_friend').show();
-      }
+	$('#friend > ul').each(function () {
+		console.log(re);
+      	if (!re.test($(this).text())) {
+      		console.log("hide");
+      		console.log(('.potential_friend').text());
+        	$('.potential_friend').hide();
+      	}
+      	else {
+      		console.log("show");
+      		console.log(('.potential_friend').text());
+        	$('.potential_friend').show();
+      	}
     });
 }); 
