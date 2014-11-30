5074913299b3ca8b81ad862059b748d24762ffef
colbyjstone <colbyjstone@gmail.com>
2014-11-30 13:40:57 -0500

    Timer Style

--- project/public/javascripts/timer.js
+++ project/public/javascripts/timer.js
@@ -1,22 +1,24 @@
+$(document).ready(function() {
 
-(function() {
+  window.startTimer = function(){
+    var element = document.getElementById('timer-container');
+    element.innerHTML = '<header id="clock-seconds"></header>';
+    var textElement = document.getElementById('clock-seconds');
 
-  "use strict";
+    var seconds = new ProgressBar.Circle(element, {
+      duration: 200,
+      color: "#008CBA",
+      trailColor: "#45ACF1",
+      strokeWidth: 8
+    });
 
-  var element = document.getElementById('timer-container');
-  element.innerHTML = '<header id="clock-seconds"></header>';
-  var textElement = document.getElementById('clock-seconds');
+    setInterval(function() {
+      var second = new Date().getSeconds();
+      seconds.animate(second / 60, function() {
+          textElement.innerHTML = second;
+      });
+    }, 1000);
+  }
 
-  var seconds = new ProgressBar.Circle(element, {
-    duration: 200,
-    color: "#008CBA",
-    trailColor: "#45ACF1",
-    strokeWidth: 5
-  });
+ });
 
-  setInterval(function() {
-    var second = new Date().getSeconds();
-    seconds.animate(second/60, function() {});
-  }, 1000);
-
-});
