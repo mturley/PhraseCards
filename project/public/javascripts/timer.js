ef7b9e8ee2733fad5f87d05c83e4e4cb8c3de422
colbyjstone <colbyjstone@gmail.com>
2014-11-30 13:01:00 -0500

    Fix timer error

--- project/public/javascripts/timer.js
+++ project/public/javascripts/timer.js
@@ -1,16 +1,22 @@
-var element = document.getElementById('timer-container');
-element.innerHTML = '<header id="clock-seconds"></header>';
-var textElement = document.getElementById('clock-seconds');
 
-var seconds = new ProgressBar.Circle(element, {
-  duration: 200,
-  color: "#008CBA",
-  trailColor: "#45ACF1",
-  strokeWidth: 5
+(function() {
+
+  "use strict";
+
+  var element = document.getElementById('timer-container');
+  element.innerHTML = '<header id="clock-seconds"></header>';
+  var textElement = document.getElementById('clock-seconds');
+
+  var seconds = new ProgressBar.Circle(element, {
+    duration: 200,
+    color: "#008CBA",
+    trailColor: "#45ACF1",
+    strokeWidth: 5
+  });
+
+  setInterval(function() {
+    var second = new Date().getSeconds();
+    seconds.animate(second/60, function() {});
+  }, 1000);
+
 });
-
-setInterval(function() {
-  var second = new Date().getSeconds();
-  seconds.animate(second/60, function() {});
-  $('.winner').addClass('animated pulse');
-}, 1000);
