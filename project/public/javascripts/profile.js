622edd4287a35f63f931354d759f16973ad2a375
Daniel <dcchoi1234@gmail.com>
2014-12-01 07:11:39 -0500

    fixed fuzzy search bug and made searching case insensitive

--- project/public/javascripts/profile.js
+++ project/public/javascripts/profile.js
@@ -1,8 +1,8 @@
 $('.potential_friend').hide();
 $('.friend-filter').keyup(function (event){
-	var re = new RegExp('^' + $('.friend-filter').val());
+	var re = new RegExp('^' + $('.friend-filter').val().toLowerCase());
 	$('#friend > li .friend_name').each(function () {
-    if (!re.test($(this).text())) {
+    if (!re.test($(this).text().toLowerCase()) || re.test("")) {
     //console.log("hide: "+$(this).text());
       $(this).parent().parent().parent().hide();
     }else {
