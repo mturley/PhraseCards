da6a89de39cf8aecbc9c2c489be1aa739e2265fe
rhromada <rhromada@umass.edu>
2014-12-03 13:39:22 -0500

    clea up stuff in profile

--- project/public/javascripts/profile.js
+++ project/public/javascripts/profile.js
@@ -3,10 +3,8 @@ $('.friend-filter').keyup(function (event){
 	var re = new RegExp('^' + $('.friend-filter').val().toLowerCase());
 	$('#friend > li .friend_name').each(function () {
     if (!re.test($(this).text().toLowerCase()) || re.test("")) {
-    //console.log("hide: "+$(this).text());
       $(this).parent().parent().parent().hide();
     }else {
-    //console.log("show: "+$(this).text());
       $(this).parent().parent().parent().show();
     }
   });
